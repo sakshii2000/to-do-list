434aac4b029c9bfa1cf2f667aaab9919e1a1776b
Sakshi Bhakta <sakshi.bhakta2021@gmail.com>
2022-08-10 15:27:24 +0530

    final version of to do list

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-let items =["Buy Food",
+const items =["Buy Food",
 "Cook Food",
 "Eat Food"];
 
-let workItems =[];
+const workItems =[];
 
 app.set('view engine', 'ejs');
 
@@ -15,15 +16,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day:"numeric",
-    month: "long"
-  }
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
     listTitle: day, newListItems: items
@@ -31,7 +25,7 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
